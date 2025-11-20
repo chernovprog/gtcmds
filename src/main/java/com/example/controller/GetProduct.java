f8d810d8347cf00c743d12b958a95fc2fd163f12
Andrii <andrii.chernov@gmail.com>
2025-11-20 18:16:11 +0200

    fix '/get_product_deatil'

--- src/main/java/com/example/controller/GetProduct.java
+++ src/main/java/com/example/controller/GetProduct.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +12,8 @@ public class GetProduct {
     return "product_1";
   }
 
-  @GetMapping("/get_product_deatil")
-  public String getProductDetail() {
-    return "product_detail_1";
+  @GetMapping("/get_product_deatil/{id}")
+  public String getProductDetail(@PathVariable(required = false) Integer id) {
+    return "product_detail_for: " + id;
   }
 }
